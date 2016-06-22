98093f750aaa2ba53c123d9b821a1b3f0e97476b
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2016-06-22 20:33:54 -0300

    Atribuição de estilos na página de login
    Atruibuição de estilo semelhante ao sistema.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	
     <meta charset="utf-8">
-    <title>AFAST ICT</title>
+    <title>Login - AFAST ICT</title>
     
     <link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -15,7 +15,7 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
 
-          <a class="col-md-3" id="icticon"><img src="img/logo_ICT1.0.png" width="60" height="64" alt=""/></a>
+          <a class="col-md-3" id="icticon"><img src="img/logo_ICT1.0.png" width="60" height="64" alt="Logo ICT"/></a>
         </div>
     
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -32,14 +32,40 @@
     </nav>
     
     
-    <main class="container-fluid" id="loader"></main>
+    <main class="container-fluid" id="loader">
+    	 <br><br><br><br>
+    	<div class="row">
+          <div class="col-md-3 "></div>
+		  <div class="col-md-3 ">
+            <img src="img/logo_AFAST1.5.png" alt="Logo AFAST" class="img-responsive img-rounded">
+          </div>
+          <div class="col-md-3 ">
+              	<br><br><br><br>
+                <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="basic-addon1">Usuário *</span>
+                    <input type="text" class="form-control" id="nome_treinador" placeholder="Nome" aria-describedby="basic-addon1">
+                </div>
+                <br><br>
+				<div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="basic-addon1">Senha *</span>
+                    <input type="password" class="form-control" id="senha_treinador" placeholder="Senha" aria-describedby="basic-addon1">
+                </div>
+                <br><br>
+                <button type="submit" class="pull-right btn btn-lg btn-default">Entrar</button>             
+          </div> 
+                
+        </div>
+        <div class="col-md-3 "></div>
+        <br><br><br><br><br>
+		<p class="text-center text-titulo col-md-12">Sistema de Gerenciamento de Frequência de Docentes</p>
+    </main>
    	
-    
+      
 	
     <footer class="mainfooter navbar-default navbar-fixed-bottom">
     	<section>
-        	<a class="col-md-3" id="afasticon"><img src="img/logo_AFAST1.5.png" width="60" height="64" alt=""/></a>
-        	<p class="text-right footertext col-md-9"> Todos os direitos reservados. </p>
+        	<!--<a class="col-md-3" id="afasticon"><img src="img/logo_AFAST1.5.png" width="60" height="64" alt=""/></a>-->
+        	<p class="text-right footertext col-md-12"> Todos os direitos reservados. </p>
         </section>    
     </footer>
 	
