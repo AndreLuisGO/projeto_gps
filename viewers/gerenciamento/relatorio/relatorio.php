3760c634a4ffceb855ffb1d8b17370dd30b9f021
Andre Luis Oliveira <algo1305@gmail.com>
2016-08-12 19:03:26 -0300

    Relatorio (arquivo faltante)

--- viewers/gerenciamento/relatorio/relatorio.php
+++ viewers/gerenciamento/relatorio/relatorio.php
@@ -5,21 +5,20 @@ require_once('../../../engine/config.php');
 	
 $MesForm = $_POST['mes'];
 $curso = $_POST['curso'];
-list($mes, $dia, $ano)=explode("/", $MesForm);
 
-$Relatorio = new Afastamento();
-$Relatorio = $Relatorio->ReadAllReport($mes,$ano,$curso);
+list($mes, $ano)=explode("/", $MesForm);
+var_dump($mes);
 
-						
+
+
 ?>
+
 <!doctype html>
 <html>
 <head>
 	
     <meta charset="utf-8">
     <title>ICT AFAST - Relatório Mensal</title>
-    
-    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 
 <body>
@@ -35,8 +34,16 @@ $Relatorio = $Relatorio->ReadAllReport($mes,$ano,$curso);
        	</section>
         <section class="col-md-3"></section>
     </section>
+	<?php
+	$Docente = new Docente();
+	$Docente = $Docente->ReadAllCurso($curso);
+	
 
-        	<table class="table table-striped table-hover">
+		?>
+        
+
+   	
+            <table class="table table-striped table-hover">
                 <thead>
                   <tr>
                     <th>Nome</th>
@@ -48,23 +55,22 @@ $Relatorio = $Relatorio->ReadAllReport($mes,$ano,$curso);
                     <th>Cód. Ocorrência</th>
                   </tr>
                 </thead>
-             
+                <?php
+             	foreach ( $Docente as $docenteRow ){
+		$Relatorio = new ClasseRelatorio();
+		$Relatorio = $Relatorio->ReadAllReport($mes,$ano,$docenteRow['id_docente']);
+		var_dump($Relatorio);
+		?>
                 <tbody>
-                	<?php
-						
-						foreach($Relatorio as $itemRow){
-							
-							
-							
-					?>
+                           
                             <tr class="">
-                                <td><?php echo $itemRow['nome_docente']; ?></td>
-                                <td><?php echo $itemRow['siape_docente']; ?></td>
-                                <td><?php echo ExibeData($itemRow['dt_inicio_afastamento']); ?></td>
-                                <td><?php echo ExibeData($itemRow['dt_fim_afastamento']); ?></td>
-                                <td><?php echo $itemRow['observ_afastamento']; ?></td>
-                                <td><?php echo $itemRow['tipo_ocorrencia']; ?></td>
-                                <td><?php echo $itemRow['codigo_ocorrencia']; ?></td>
+                                <td><?php echo $docenteRow['nome_docente']; ?></td>
+                                <td><?php echo $docenteRow['siape_docente']; ?></td>
+                                <td><?php echo $docenteRow['dt_inicio_afastamento']; ?></td>
+                                <td><?php echo $docenteRow['dt_fim_afastamento']; ?></td>
+                                <td><?php echo $docenteRow['observ_afastamento']; ?></td>
+                                <td><?php echo $docenteRow['tipo_ocorrencia']; ?></td>
+                                <td><?php echo $docenteRow['codigo_ocorrencia']; ?></td>
                             </tr>
                     <?php
 						}
